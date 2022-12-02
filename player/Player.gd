2b63f6581b62ace63e88bbd114cc4b80b2c53d1e
JW <73392543+james-w1@users.noreply.github.com>
2022-12-02 21:00:56 +0000

    uwu direction

--- player/Player.gd
+++ player/Player.gd
@@ -3,32 +3,64 @@ extends KinematicBody2D
 export (int) var speed = 200
 
 var velocity = Vector2()
+var last_direction = Vector2(0, 1)
 
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
-		$AnimatedSprite.play('Up')
 		velocity.x += 1
 	if Input.is_action_pressed("left"):
-		$AnimatedSprite.play('Up')
 		velocity.x -= 1
 	if Input.is_action_pressed("down"):
-		$AnimatedSprite.play('Down')
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
-		$AnimatedSprite.play('Up')
 		velocity.y -= 1
+
 	velocity = velocity.normalized() * speed
+	
+func vector_to_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	if norm_direction.y >= 0.707:
+		return "down"
+	elif norm_direction.y <= -0.707:
+		return "up"
+	elif norm_direction.x <= -0.707:
+		return "left"
+	elif norm_direction.x >= 0.707:
+		return "right"
+	return "down"
+	
+func animate_gamer(direction: Vector2):
+	if direction != Vector2.ZERO: # if moving
+		# update last_direction
+		last_direction = direction
+		
+		# Match the last direction to the way the player is facing and flip
+		match vector_to_direction(last_direction):
+			"down":
+				$AnimatedSprite.play("down")
+				$AnimatedSprite.flip_h = false
+			"up":
+				$AnimatedSprite.play("up")
+				$AnimatedSprite.flip_h = false
+			"right":
+				$AnimatedSprite.play("down")
+				$AnimatedSprite.flip_h = false
+			"left":
+				$AnimatedSprite.play("down")
+				$AnimatedSprite.flip_h = true
+	else: #if not moving
+		$AnimatedSprite.play("default")
 
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	animate_gamer(velocity) # animate the player character
+	velocity = move_and_slide(velocity) # move the player character
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	
