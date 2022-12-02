0f5561fc5ce689138c534867b105c3fe9c2e2a63
Jems <jamesgmcw@gmail.com>
2022-12-02 19:49:33 +0000

    bingus bongus

---
+++ world/World.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+const Player = preload("res://src/Player/Player.tscn")
+const Exit = preload("res://src/World/Exit.tscn")
+
+var borders = Rect2(-112, -80, 238, 160)
+
+onready var tileMap = $TileMap
+
+
+func _ready():
+	randomize()
+	generate_level()
+
+func generate_level():
+	var walker = Walker.new(Vector2(29, 16), borders)
+	var map = walker.walk(200)
+	
+	var player = Player.instance()
+	add_child(player)
+	player.position = map.front() * 32
+	
+	var new_weapon = preload("res://src/Items/Weapons/Melee/Longsword.tscn").instance()
+	add_child(new_weapon)
+	new_weapon.position = map.front() * 32
+	
+	var exit = Exit.instance()
+	add_child(exit)
+	exit.position = walker.get_end_room().position * 32
+	exit.connect("leaving_level", self, "reload_level")
+	
+	walker.queue_free()
+	for location in map:
+		tileMap.set_cellv(location, -1)
+	tileMap.update_bitmask_region(borders.position, borders.end)
+
+func reload_level():
+	get_tree().reload_current_scene()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		reload_level()
